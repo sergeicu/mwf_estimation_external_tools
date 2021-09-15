8f15af73234dbff7a0d13dd5e85ded1e57bcd069
Serge <serge.vasylechko@tch.harvard.edu>
2021-09-15 14:05:39 -0400

    Updated readme and added example script

---
+++ EXAMPLE.sh
@@ -0,0 +1,63 @@
+# Warning: all PHI data was substituted with <REMOVED> flag 
+
+# Patient details
+MRN:<REMOVED>
+Date:<REMOVED>
+
+# Get code 
+git clone git@github.com:sergeicu/mwf_estimation_external_tools.git
+cd mwf_estimation_external_tools/
+codedir=$(pwd)
+
+# Initialize correct conda environment 
+# (prereqs: see email sent to Onur and Sila on 20210914 with title: "To use my conda (and newer python)")
+# this loads the correct version of python and code dependencies
+conda activate tch2_yml 
+
+# Create new directory 
+rootdir=/fileserver/fastscratch/serge/s20210815_mwf_example_processing_for_onur  # root folder in which all the processed data will be stored 
+cp -r $codedir/* $rootdir/ # copy all our code here
+experiment=<REMOVED> # subfolder that identifies current experiment (e.g. today's date)
+mkdir $rootdir/$experiment
+cd $rootdir/$experiment
+
+# GET DICOMS 
+MRN=<REMOVED>
+DATE=<REMOVED> 
+savedir=$rootdir/$experiment/
+bash retrieve2.sh $MRN $DATE $savedir
+
+# DICOM2NRRD
+session=<REMOVED> # a child folder that retrieve.sh script creates when it fetches dicoms. This corresponds to MRI session id 
+scan=7_myelin_1x1x4 # name of a particular scan that we are interested in
+scandir=$rootdir/$experiment/$MRN/$session//$scan
+python dicom_to_nrrd.py --dicomdir $scandir
+
+# FIX YAML 
+nrrd=$scandir/NRRD/
+python fix_yaml.py --input $nrrd
+
+# NRRD2NIFTI **
+nrrd_fixed=$scandir/NRRD/
+python nrrd_to_nifti.py --input $nrrd_fixed
+
+# run julia (prereqs: installed julia language on your machine)
+julia_out=$scandir/NRRD/out_julia
+mkdir $julia_out
+cp -r $scandir/NRRD/*.nii.gz $julia_out/
+python run_julia.py --input $julia_out/*.nii.gz
+
+# (if not downloaded) download anima docker image (prereqs: installed docker software, and sudo access to the machine)
+name=sergeicu/anima:latest
+sudo docker pull $name
+
+# run anima 
+anima_out=$scandir/NRRD/out_anima
+mkdir $anima_out
+cp -r $scandir/NRRD/*.nii.gz $anima_out/
+localfolder=$anima_out # will grab all .nii.gz images
+sudo docker run -it --rm -v $localfolder:/data sergeicu/anima:latest /bin/bash
+python run_anima.py /data/
+exit
+
+# ** = do not use direct `dicom to nifti` converter tool because the header is not preserved correctly. 
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # mwf_estimation_external_tools
+
+Estimate Myelin Water Fraction (MWF) maps with: 
+1. DECAES toolbox 
+https://juliahub.com/ui/Packages/DECAES/LsYv9/0.4.1
+2. Anima toolbox 
+https://anima.readthedocs.io/en/latest/
+
+See instructions in `EXAMPLE.sh` file 
+
+
+## About DECAES: 
+This is a fast implementation of the original MWF estimation method proposed by Prasloski et al in 2012. 
+"Applications of stimulated echo correction to multicomponent T2 analysis"
+https://onlinelibrary.wiley.com/doi/full/10.1002/mrm.23157
+
+DECAES is written with Julia language and is ~10x faster (or more) than the original MATLAB implementation.
+
+NB: 
+Original MATLAB code was written by Thomas Prasloski (email: tprasloski@gmail.com). Modifications to the MATLAB code were made by Vanessa Wiggermann to enable processing on various MATLAB versions in February 2019. The Julia port is based on this modified version
+Prasloski's 2012 algorithm - fast implementation with Julia 
+
+## About Anima: 
+
+This is a C++ implementation of the original MWF estimation method proposed by Chatterjee et al 2018. 
+"Multi-compartment model of brain tissues from T2 relaxometry MRI using gamma distribution"
+https://hal.archives-ouvertes.fr/hal-01744852/document
+
+Anima software binaries are available from its website for linux Ubuntu distribution. 
+
+We had wrapped these binaries in a docker image, that can be run on any OS. 
+https://github.com/sergeicu/anima-docker
